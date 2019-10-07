6e86913223745bb0ef6b0635b2fd360541b01fdf
AGbetaryal <920291780@qq.com>
2019-10-07 22:51:48 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+<!--    <img src="./assets/logo.png">-->
+<!--    <router-view/>-->
     <router-view/>
+    <FooterGuider v-show="$route.meta.showFooter"/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import FooterGuider from "./components/FooterGuider/FooterGuider"
+  import {mapActions} from 'vuex'
+  export default {
+    mounted () {
+      // this.$store.dispatch('getAddress')
+      this.getAddress()
+      this.getUserInfo()
+    },
+
+    methods: {
+      ...mapActions(['getAddress', 'getUserInfo'])
+    },
+    components: {
+      FooterGuider
+    }
+  /*  name: 'App'*/
+  }
 </script>
 
-<style>
-#app {
+<style lang="stylus" rel="stylesheet/stylus">
+/*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
